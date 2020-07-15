ecac5505d6eb9ac00d5748258bc9839f35a11b37
JakeQuilty <jacob.quilty@gmail.com>
2020-07-15 08:28:14 -0400

    update script

--- bin/list-private-registry.sh
+++ bin/list-private-registry.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-http_proxy=secretless_server:8021 curl hub.docker.com/v2/repositories/jacobquilty/?page_size=10000
+http_proxy=secretless_server:8021 curl 'hub.docker.com/v2/repositories/jacobquilty/?page_size=10000'
