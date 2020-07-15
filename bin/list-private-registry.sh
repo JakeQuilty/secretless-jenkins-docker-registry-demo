3347d34fde25d6d8662ba67b955894d314722e80
JakeQuilty <jacob.quilty@gmail.com>
2020-07-15 09:37:58 -0400

    script

--- bin/list-private-registry.sh
+++ bin/list-private-registry.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "Retrieving repo list..."
 http_proxy=secretless_server:8021 curl 'hub.docker.com/v2/repositories/jacobquilty/?page_size=10000' > repos
+echo "Repos:"
 cat repos | jq
