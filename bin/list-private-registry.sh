e63b940f049ee42ae0a11be87d1f1578ef9bc88b
JakeQuilty <jacob.quilty@gmail.com>
2020-07-15 09:26:28 -0400

    script

--- bin/list-private-registry.sh
+++ bin/list-private-registry.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-http_proxy=secretless_server:8021 curl 'hub.docker.com/v2/repositories/jacobquilty/?page_size=10000' | jq
+REPOS="$(http_proxy=secretless_server:8021 curl 'hub.docker.com/v2/repositories/jacobquilty/?page_size=10000')"
+echo $REPOS | jq
